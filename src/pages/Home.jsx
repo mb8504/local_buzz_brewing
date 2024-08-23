88c74e540023916be7d45286d1bf0a6e46e16bd7
mb8504 <mbergmarketing@gmail.com>
2024-08-22 21:22:22 -0400

    added pagination

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,16 @@
 import React, { useRef, useState } from 'react';
-// Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
-
-// Import Swiper styles
 import 'swiper/css';
-import 'swiper/css/navigation';
-
-// import required modules
-import { Navigation } from 'swiper/modules';
+import 'swiper/css/pagination';
+import { Pagination } from 'swiper/modules';
 
 export default function App() {
   return (
     <>
-      <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
-        <SwiperSlide><img src="/1600_944_hero1.png" alt="" /></SwiperSlide>
-        <SwiperSlide><img src="/1600_944_hero2.png" alt="" /></SwiperSlide>
-        <SwiperSlide><img src="/1600_944_hero3.png" alt="" /></SwiperSlide>
+      <Swiper spaceBetween={30} pagination={{clickable: true,}} modules={[Pagination]} className="mySwiper sliderCustomHeight">
+        <SwiperSlide><img className='h-auto w-full' src="/1600_944_hero1.png" alt="" /></SwiperSlide>
+        <SwiperSlide><img className='h-auto w-full' src="/1600_944_hero2.png" alt="" /></SwiperSlide>
+        <SwiperSlide><img className='h-auto w-full' src="/1600_944_hero3.png" alt="" /></SwiperSlide>
         {/* <SwiperSlide>Slide 4</SwiperSlide>
         <SwiperSlide>Slide 5</SwiperSlide>
         <SwiperSlide>Slide 6</SwiperSlide>
