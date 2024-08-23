f194ecd3b60229d1ab22d7dba58a7ae6080a6094
mb8504 <mbergmarketing@gmail.com>
2024-08-23 15:25:55 -0400

    Added slider transition

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,21 +2,28 @@ import React, { useRef, useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/pagination';
-import { Pagination } from 'swiper/modules';
+// import 'swiper/css/navigation';
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+
 
 export default function App() {
   return (
     <>
-      <Swiper spaceBetween={30} pagination={{clickable: true,}} modules={[Pagination]} className="mySwiper sliderCustomHeight">
-        <SwiperSlide><img className='h-auto w-full' src="/1600_944_hero1.png" alt="" /></SwiperSlide>
-        <SwiperSlide><img className='h-auto w-full' src="/1600_944_hero2.png" alt="" /></SwiperSlide>
-        <SwiperSlide><img className='h-auto w-full' src="/1600_944_hero3.png" alt="" /></SwiperSlide>
-        {/* <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide> */}
+      <Swiper spaceBetween={30} autoplay={{delay: 7500,disableOnInteraction: false,}} pagination={{clickable: true,}} modules={[Autoplay, Pagination]} className="mySwiper sliderCustomHeight">
+        <SwiperSlide>
+          <img className='h-auto w-full sliderText1' src="/1600_944_hero1.png" alt="" />
+          <div className='absolute top-24 left-14'>
+            <h1 className=''>
+              THIS IS MY HERO 1 TEXT
+            </h1>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img className='h-auto w-full' src="/1600_944_hero2.png" alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img className='h-auto w-full' src="/1600_944_hero3.png" alt="" />
+        </SwiperSlide>
       </Swiper>
     </>
   );
