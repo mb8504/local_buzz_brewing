557e2f4a811939db35c18bb704693b9a8f56f153
mb8504 <mbergmarketing@gmail.com>
2024-08-25 18:08:32 -0400

    finished drink carousel

--- src/pages/Home/components/BeerCarousel.jsx
+++ src/pages/Home/components/BeerCarousel.jsx
@@ -1,11 +1,10 @@
-import React, { useRef, useState } from 'react';
-// Import Swiper React components
+import React, { useRef, useState, useEffect } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-// Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/pagination';
-// import required modules
 import { Pagination } from 'swiper/modules';
+
+
 
 export default function BeerCarousel() {
   return (
@@ -23,15 +22,24 @@ export default function BeerCarousel() {
         modules={[Pagination]}
         className="mySwiper"
       >
-        <SwiperSlide><img src="/33_carousel_beer.png" alt="" /></SwiperSlide>
-        <SwiperSlide><img src="/33_carousel_beer.png" alt="" /></SwiperSlide>
-        <SwiperSlide><img src="/33_carousel_beer.png" alt="" /></SwiperSlide>
-        <SwiperSlide><img src="/33_carousel_beer.png" alt="" /></SwiperSlide>
-        <SwiperSlide><img src="/33_carousel_beer.png" alt="" /></SwiperSlide>
-        <SwiperSlide><img src="/33_carousel_beer.png" alt="" /></SwiperSlide>
-        <SwiperSlide><img src="/33_carousel_beer.png" alt="" /></SwiperSlide>
-        <SwiperSlide><img src="/33_carousel_beer.png" alt="" /></SwiperSlide>
-        <SwiperSlide><img src="/33_carousel_beer.png" alt="" /></SwiperSlide>
+        <SwiperSlide><img src="/33_carousel_beer.png" alt="" /><div className='absolute inset-0 transition-opacity duration-300 opacity-0 hover:opacity-100 hover:bg-slate-700/75 z-10 flex items-center justify-center'><h3 className='text-white py-6 px-3 mx-auto text-xl'>Hazy IPA</h3></div>
+        </SwiperSlide>
+        <SwiperSlide><img src="/33_carousel_beer.png" alt="" /><div className='absolute inset-0 transition-opacity duration-300 opacity-0 hover:opacity-100 hover:bg-slate-700/75 z-10 flex items-center justify-center'><h3 className='text-white py-6 px-3 mx-auto text-xl'>Pilsner</h3></div>
+        </SwiperSlide>
+        <SwiperSlide><img src="/33_carousel_beer.png" alt="" /><div className='absolute inset-0 transition-opacity duration-300 opacity-0 hover:opacity-100 hover:bg-slate-700/75 z-10 flex items-center justify-center'><h3 className='text-white py-6 px-3 mx-auto text-xl'>West Coast IPA</h3></div>
+        </SwiperSlide>
+        <SwiperSlide><img src="/33_carousel_beer.png" alt="" /><div className='absolute inset-0 transition-opacity duration-300 opacity-0 hover:opacity-100 hover:bg-slate-700/75 z-10 flex items-center justify-center'><h3 className='text-white py-6 px-3 mx-auto text-xl'>Oatmeal Stout</h3></div>
+        </SwiperSlide>
+        <SwiperSlide><img src="/33_carousel_beer.png" alt="" /><div className='absolute inset-0 transition-opacity duration-300 opacity-0 hover:opacity-100 hover:bg-slate-700/75 z-10 flex items-center justify-center'><h3 className='text-white py-6 px-3 mx-auto text-xl'>Leamonade Shandy</h3></div>
+        </SwiperSlide>
+        <SwiperSlide><img src="/33_carousel_beer.png" alt="" /><div className='absolute inset-0 transition-opacity duration-300 opacity-0 hover:opacity-100 hover:bg-slate-700/75 z-10 flex items-center justify-center'><h3 className='text-white py-6 px-3 mx-auto text-xl'>Lager</h3></div>
+        </SwiperSlide>
+        <SwiperSlide><img src="/33_carousel_beer.png" alt="" /><div className='absolute inset-0 transition-opacity duration-300 opacity-0 hover:opacity-100 hover:bg-slate-700/75 z-10 flex items-center justify-center'><h3 className='text-white py-6 px-3 mx-auto text-xl'>Seasonal Ale</h3></div>
+        </SwiperSlide>
+        <SwiperSlide><img src="/33_carousel_beer.png" alt="" /><div className='absolute inset-0 transition-opacity duration-300 opacity-0 hover:opacity-100 hover:bg-slate-700/75 z-10 flex items-center justify-center'><h3 className='text-white py-6 px-3 mx-auto text-xl'>New England IPA</h3></div>
+        </SwiperSlide>
+        <SwiperSlide><img src="/33_carousel_beer.png" alt="" /><div className='absolute inset-0 transition-opacity duration-300 opacity-0 hover:opacity-100 hover:bg-slate-700/75 z-10 flex items-center justify-center'><h3 className='text-white py-6 px-3 mx-auto text-xl'>Double Dry Hopped IPA</h3></div>
+        </SwiperSlide>
       </Swiper>
       <div className='flex justify-center mt-40'>
         <button className='bg-slate-100 text-[#145769] p-4 text-xl rounded-3xl hover:bg-[#145769] hover:text-white'>SEE OUR BEERS</button>
